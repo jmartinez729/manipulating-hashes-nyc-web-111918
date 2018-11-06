dd85fdc849c04692ae7434864b482b1a03d09624
John Martinez <jmartinez729@gmail.com>
2018-11-06 13:55:41 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,8 +17,7 @@ def first_challenge
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
-        value.delete_if { 
-        end 
+        value.delete_if { |flavor| flavor == "strawberry" }
       end 
     end 
   end 
