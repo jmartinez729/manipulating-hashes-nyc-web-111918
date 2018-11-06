a09061111f6ba245eb175b93281e513a3986073f
John Martinez <jmartinez729@gmail.com>
2018-11-06 13:56:18 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -21,13 +21,6 @@ def first_challenge
       end 
     end 
   end 
-  
-  contacts.each do |person, data|
-    data.each do |attribute, value|
-      if attribute == :favorite_icecream_flavors
-        attribute.delete_if {
-
-  #remember to return your newly altered contacts hash!
   contacts
 end
 
