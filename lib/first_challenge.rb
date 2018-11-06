2169de76812362a6a48b70f9029858dd0e39c855
John Martinez <jmartinez729@gmail.com>
2018-11-06 13:50:43 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,20 @@ def first_challenge
   }
 
   #your code here
-
+  contacts.each do |person, data|
+    data.each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+        value.each do |flavor|
+          puts "#{flavor}"
+        end 
+      end 
+    end 
+  end 
+  
+  contacts.each do |person, data|
+    data.each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+        attribute.delete_if {
 
   #remember to return your newly altered contacts hash!
   contacts
